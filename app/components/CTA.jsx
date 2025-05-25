0a3d0427d92f86415e75c3e674987411f7e311b6
ahsan <makabir98@yahoo.com>
2025-05-25 22:23:19 +0600

    mobile updated

--- app/components/CTA.jsx
+++ app/components/CTA.jsx
@@ -1,19 +1,33 @@
 import { FaCheckCircle } from "react-icons/fa";
 const CTA = () => {
     return (
-        <div className="w-full px-20 py-16" style={{background: `linear-gradient(to right, rgba(88, 158, 89, 1) 0%, rgba(31, 56, 32, 1) 100%`}}>
-            <div className="flex items-center justify-evenly">
-                <h2 className="text-5xl font-medium text-[#F7F7F7]">Leading Way In <br/>Building & Civil<br/>Construction</h2>
+        <div className="w-full px-8 py-6" style={{background: `linear-gradient(to right, rgba(88, 158, 89, 1) 0%, rgba(31, 56, 32, 1) 100%`}}>
+            <div className="flex flex-col gap-4 items-center justify-evenly">
+                <h2 className="text-2xl text-center font-medium text-[#F7F7F7]">Leading Way In Building & Civil Construction</h2>
                 <ul className="text-[#F7F7F7]">
-                    <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle size={28}/></span>Professional Staff</li>
-                    <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle  size={28}/></span>100% Satisfaction</li>
-                    <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle  size={28}/></span>Accurate Testing</li>
-                    <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle  size={28}/></span>Transparent Pricing</li>
+                    <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle size={16}/></span>Professional Staff</li>
+                    <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle  size={16}/></span>100% Satisfaction</li>
+                    <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle  size={16}/></span>Accurate Testing</li>
+                    <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle  size={16}/></span>Transparent Pricing</li>
                 </ul>
-                <button className="rounded-full bg-[#FF7420] hover:bg-[#FF7420]/30 w-[200px] h-[72px] p-auto font-bold text-2xl text-[#F7F7F7] hover:text-[hsl(24,87%,72%)] ring-2 ring-[#EBDBA5] shadow-2xl shadow-[#FF7420] hover:shadow-none hover:border-4">Contact us</button>
+                <button className="rounded-full bg-[#FF7420] hover:bg-[#FF7420]/30 px-6 py-2 font-bold text-xl text-[#F7F7F7] hover:text-[hsl(24,87%,72%)] ring-2 ring-[#EBDBA5] shadow-2xl shadow-[#FF7420] hover:shadow-none hover:border-4">Contact us</button>
             </div>
         </div>
     );
+    // return (
+    //     <div className="w-full px-20 py-16" style={{background: `linear-gradient(to right, rgba(88, 158, 89, 1) 0%, rgba(31, 56, 32, 1) 100%`}}>
+    //         <div className="flex items-center justify-evenly">
+    //             <h2 className="text-5xl font-medium text-[#F7F7F7]">Leading Way In <br/>Building & Civil<br/>Construction</h2>
+    //             <ul className="text-[#F7F7F7]">
+    //                 <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle size={28}/></span>Professional Staff</li>
+    //                 <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle  size={28}/></span>100% Satisfaction</li>
+    //                 <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle  size={28}/></span>Accurate Testing</li>
+    //                 <li className="flex gap-3 font-bold items-center"><span className="text-[#FF7420] mt-1"><FaCheckCircle  size={28}/></span>Transparent Pricing</li>
+    //             </ul>
+    //             <button className="rounded-full bg-[#FF7420] hover:bg-[#FF7420]/30 w-[200px] h-[72px] p-auto font-bold text-2xl text-[#F7F7F7] hover:text-[hsl(24,87%,72%)] ring-2 ring-[#EBDBA5] shadow-2xl shadow-[#FF7420] hover:shadow-none hover:border-4">Contact us</button>
+    //         </div>
+    //     </div>
+    // );
 };
 
 export default CTA;
--- app/components/WhatWeDo.jsx
+++ app/components/WhatWeDo.jsx
@@ -5,10 +5,10 @@ import stockPhoto2 from "../images/stock photo 2.jpg";
 
 const WhatWeDo = () => {
     return (
-        <div className="relative w-full px-20 py-16 bg-[#135120]/8">
+        <div className="relative w-full px-8 py-6 bg-[#135120]/8">
             <h2 className="text-5xl font-medium underline decoration-[#F3692A] decoration-4 underline-offset-8">What we do</h2>
-            <div className="flex justify-center items-center pt-12">
-                <ul className="w-1/2 text-xl">
+            <div className="flex justify-center pt-6">
+                <ul className="w-1/2 text-lg">
                     <li>Rajuk Plan Approval</li>
                     <li>Civil Works Construction</li>
                     <li>MEP Drawing, Design and Installation</li>
@@ -21,18 +21,47 @@ const WhatWeDo = () => {
                         src={stockPhoto2}
                         width={200}
                         alt="Photo by Roberto Nickson on Unsplash"
-                        className="absolute top-[-200px] left-50"
+                        className="absolute top-[20px] left-5 z-[-3]"
                     />
                     <Image
                         src={stockPhoto1}
                         width={500}
                         alt="construction"
-                        className="absolute top-[-100px] left-[-250px] z-[-1]"
+                        className="absolute top-[-10px] left-[-8] z-[-1]"
                     />
                 </div>
             </div>
         </div>
     );
+    // return (
+    //     <div className="relative w-full px-20 py-16 bg-[#135120]/8">
+    //         <h2 className="text-5xl font-medium underline decoration-[#F3692A] decoration-4 underline-offset-8">What we do</h2>
+    //         <div className="flex justify-center items-center pt-12">
+    //             <ul className="w-1/2 text-xl">
+    //                 <li>Rajuk Plan Approval</li>
+    //                 <li>Civil Works Construction</li>
+    //                 <li>MEP Drawing, Design and Installation</li>
+    //                 <li>Engineering Consultancy</li>
+    //                 <li>Architectrual Drawing</li>
+    //                 <li>Interior and Exterior Decoration Works</li>
+    //             </ul>
+    //             <div className="relative w-1/2">
+    //                 <Image
+    //                     src={stockPhoto2}
+    //                     width={200}
+    //                     alt="Photo by Roberto Nickson on Unsplash"
+    //                     className="absolute top-[-200px] left-50"
+    //                 />
+    //                 <Image
+    //                     src={stockPhoto1}
+    //                     width={500}
+    //                     alt="construction"
+    //                     className="absolute top-[-100px] left-[-250px] z-[-1]"
+    //                 />
+    //             </div>
+    //         </div>
+    //     </div>
+    // );
 };
 
 export default WhatWeDo;
