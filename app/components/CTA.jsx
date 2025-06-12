acb9ebeb86e7ef707558945f0ca0a81c6a89a9eb
ahsan <makabir98@yahoo.com>
2025-06-12 08:24:28 +0600

    CTA button linked with contact page in the MVP

--- app/components/CTA.jsx
+++ app/components/CTA.jsx
@@ -1,5 +1,10 @@
+'use client';
+
 import { FaCheckCircle } from "react-icons/fa";
+import { useRouter } from 'next/navigation';
 const CTA = () => {
+    const router = useRouter();
+    
     return (
         <div className="w-full px-6 sm:px-10 md:px-10 lg:px-40 py-10 md:py-16 text-center" style={{background: `linear-gradient(to right, rgba(88, 158, 89, 1) 0%, rgba(31, 56, 32, 1) 100%`}}>
             <div className="flex flex-col md:flex-row items-center justify-between gap-10 2xl:w-3/4 2xl:mx-auto">
@@ -26,7 +31,9 @@ const CTA = () => {
                 </ul>
 
                 {/* CTA Button */}
-                <button className="rounded-full bg-[#FF7420] hover:bg-[#FF7420]/30 px-6 py-2 lg:px-10 lg:py-4 font-bold text-lg lg:text-xl text-[#F7F7F7] hover:text-[hsl(24,87%,72%)] ring-2 ring-[#EBDBA5] shadow-2xl shadow-[#FF7420] hover:shadow-none hover:border-4 transition">
+                <button 
+                    onClick={() => router.push('/contact')} 
+                    className="rounded-full bg-[#FF7420] hover:bg-[#FF7420]/30 px-6 py-2 lg:px-10 lg:py-4 font-bold text-lg lg:text-xl text-[#F7F7F7] hover:text-[hsl(24,87%,72%)] ring-2 ring-[#EBDBA5] shadow-2xl shadow-[#FF7420] hover:shadow-none hover:border-4 transition">
                     Contact us
                 </button>
             </div>
