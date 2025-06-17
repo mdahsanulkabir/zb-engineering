35176bc9d2411808337f60df2dda19a57629bb36
ahsan <makabir98@yahoo.com>
2025-06-17 22:10:30 +0600

    navbar and footer company name styling updated.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
                         width={48}
                         height={48}
                     />
-                    <p className="font-bold text-xl">ZB Engineering <br className="hidden min-[860px]:block min-[1065px]:hidden" />& Technology</p>
+                    <p className="font-bold text-base min-[470px]:text-xl">ZB Engineering <br className="hidden min-[860px]:block min-[1065px]:hidden" />& Technology</p>
                 </div>
 
                 {/* Desktop Nav */}
@@ -89,39 +89,6 @@ const Navbar = () => {
             }
         </>
     );
-    // return (
-    //     <div className="flex items-center justify-between px-20 py-8 bg-gray-800 text-white">
-    //         <div className="flex items-center space-x-5">
-    //             <Image
-    //                 src={logo}
-    //                 alt="ZB Engineering Logo"
-    //                 width={72}
-    //                 height={72}
-    //             // className="logo"
-    //             />
-    //             <p className="font-bold text-2xl">ZB Engineering & Technology</p>
-    //         </div>
-    //         <div>
-    //             <ul className="flex space-x-4">
-    //                 <Link href="/" className={`hover:text-gray-400 cursor-pointer ${pathname === "/" ? "text-[#F3692A]" : ""}`}>Home</Link>
-    //                 <Link href="/about" className={`hover:text-gray-400 cursor-pointer ${pathname === "/about" ? "text-[#F3692A]" : ""}`}>About</Link>
-    //                 <Link href="/projects" className={`hover:text-gray-400 cursor-pointer ${pathname === "/projects" ? "text-[#F3692A]" : ""}`}>Projects</Link>
-    //                 <Link href="/services" className={`hover:text-gray-400 cursor-pointer ${pathname === "/services" ? "text-[#F3692A]" : ""}`}>Services</Link>
-    //                 <Link href="/contact" className={`hover:text-gray-400 cursor-pointer ${pathname === "/contact" ? "text-[#F3692A]" : ""}`}>Contact</Link>
-    //                 <li className="hover:text-gray-400 cursor-pointer">|</li>
-    //                 <a
-    //                     href="https://www.facebook.com/profile.php?id=61561311471666"
-    //                     target="_blank"
-    //                     rel="noopener noreferrer"
-    //                     className="hover:text-gray-400 cursor-pointer"
-    //                 >
-    //                     <FaFacebook size="24px" />
-    //                 </a>
-    //                 <Link href="#" className="hover:text-gray-400 cursor-pointer">{<FaLinkedin size='24px' />}</Link>
-    //             </ul>
-    //         </div>
-    //     </div>
-    // );
 };
 
 export default Navbar;
