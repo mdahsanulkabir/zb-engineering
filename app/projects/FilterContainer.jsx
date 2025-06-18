c3fa3f9f672e7d8554353ffa3d590deabc2c2a77
ahsan <makabir98@yahoo.com>
2025-06-18 09:05:53 +0600

    project filter logic added

--- app/projects/FilterContainer.jsx
+++ app/projects/FilterContainer.jsx
@@ -1,13 +1,60 @@
+'use client'
+
+import { useRef } from "react";
+
+const FilterContainer = ({ projects, setAllProjects, setShowNotFound }) => {
+    const serviceRef = useRef();
+    const stageRef = useRef();
+    const districtRef = useRef();
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+
+        // Reset the not found state
+        setShowNotFound(false);
 
 
-const FilterContainer = () => {
+        const selectedService = serviceRef.current.value;
+        const selectedStage = stageRef.current.value;
+        const selectedDistrict = districtRef.current.value;
+
+        console.log({selectedDistrict, selectedService, selectedStage});
+
+        // Filter projects based on selected criteria
+
+        let filtered = projects;
+
+        if (selectedService) {
+            filtered = filtered.filter(
+                project => project.serviceType.includes(Number(selectedService))
+            );
+        }
+
+        if (selectedStage) {
+            filtered = filtered.filter(
+                project => project.stage === selectedStage
+            );
+        }
+
+        if (selectedDistrict) {
+            filtered = filtered.filter(
+                project => project.district === selectedDistrict
+            );
+        }
+
+        // If no projects match the criteria, set not found state
+        filtered.length === 0 && setShowNotFound(true);
+        
+        // Update the state with the filtered projects
+        setAllProjects(filtered);
+    };
     return (
-        <div className="project-filter-container">
+        <form onSubmit={handleSearch} className="project-filter-container">
 
             {/* category filter */}
             <div className="project-filter">
-                <label htmlFor="category" className="">Category</label>
-                <select name="category" id="category">
+                <label htmlFor="service" className="">Services</label>
+                <select ref={serviceRef} name="service" id="service">
                     <option value="" className="text-center">--Select category--</option>
                     <option value="1">Rajuk Plan Approval</option>
                     <option value="2">Structural Design</option>
@@ -22,7 +69,7 @@ const FilterContainer = () => {
             {/* stage filter */}
             <div className="project-filter">
                 <label htmlFor="stage">Stage of Work</label>
-                <select name="stage" id="stage">
+                <select ref={stageRef} name="stage" id="stage">
                     <option value="" className="text-center">--Select stage--</option>
                     <option value="completed">Completed</option>
                     <option value="on-going">On Going</option>
@@ -32,18 +79,18 @@ const FilterContainer = () => {
             {/* location filter */}
             <div className="project-filter">
                 <label htmlFor="location">Location</label>
-                <select name="location" id="location">
+                <select ref={districtRef} name="location" id="location">
                     <option value="" className="text-center">--Select location--</option>
-                    <option value="dhaka">Dhaka</option>
+                    <option value="Dhaka">Dhaka</option>
                     <option value="Pabna">Pabna</option>
-                    <option value="Kustia">Kustia</option>
+                    <option value="Kushtia">Kushtia</option>
                     <option value="Barishal">Barishal</option>
                 </select>
             </div>
             <div className="project-filter-search-container">
-                <button >Search</button>
+                <button type='submit'>Search</button>
             </div>
-        </div>
+        </form>
     );
 };
 
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -8,6 +8,7 @@ const page = () => {
     const [selectedProject, setSelectedProject] = useState(null);
 
     const [allProjects, setAllProjects] = useState(null);
+    const [showNotFound, setShowNotFound] = useState(false);
 
     useEffect(() => {
         setAllProjects(projects);
@@ -52,42 +53,60 @@ const page = () => {
                 We have successfully completed numerous projects, each a testament to our commitment to quality and innovation. Our portfolio includes residential complexes, commercial buildings, and infrastructure projects that have transformed the landscape of Bangladesh.
             </p>
 
-            <FilterContainer />
+            <FilterContainer projects={projects} setAllProjects={setAllProjects} setShowNotFound={setShowNotFound} />
 
             {/* filter results here */}
             <div className="w-full py-6">
                 <h2 className="text-2xl font-medium underline decoration-[#F3692A] decoration-4 underline-offset-8">Projects</h2>
-                <div className="project-cards-container flex flex-wrap gap-6">
-                    {/* Project Cards */}
-                    {
-                        allProjects?.map((project) => (
-                            <div
-                                key={project.id}
-                                className="relative w-[200px] bg-gray-300/50 rounded-lg shadow-lg shadow-gray-400 transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:ring-2 hover:ring-[#F3692A] hover:ring-offset-2 hover:ring-offset-gray-200 cursor-pointer"
-                                onClick={() => handleCardClick(project)}
-                            >
-                                <Image
-                                    src={project.image}
-                                    alt={project.name}
-                                    height={400}
-                                    width={300}
-                                    className="object-cover rounded-lg h-[266px]!"
-                                />
-                                <div
-                                    className="absolute top-0 w-full h-[266px] pointer-events-none z-20 rounded-b-lg"
-                                    style={{
-                                        background: `linear-gradient(to top, rgba(17, 17, 17, 0.8) 0%, rgba(17, 17, 17, 0.5) 15%, rgba(17, 17, 17, 0) 20%)`,
-                                    }}
-                                >
-                                    <div className="absolute bottom-2 left-0 right-0">
-                                        <p className="text-white font-semibold text-center">{project.name}</p>
-                                        <p className="text-white text-xs text-center">{`${project.location[project.location.length - 1]}, ${project.district}`}</p>
+                {
+                    allProjects && allProjects.length > 0 && (
+                        <div className="project-cards-container flex flex-wrap gap-6">
+                            {/* Project Cards */}
+                            {
+                                allProjects?.map((project) => (
+                                    <div
+                                        key={project.id}
+                                        className="relative w-[200px] bg-gray-300/50 rounded-lg shadow-lg shadow-gray-400 transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-2xl hover:ring-2 hover:ring-[#F3692A] hover:ring-offset-2 hover:ring-offset-gray-200 cursor-pointer"
+                                        onClick={() => handleCardClick(project)}
+                                    >
+                                        <Image
+                                            src={project.image}
+                                            alt={project.name}
+                                            height={400}
+                                            width={300}
+                                            className="object-cover rounded-lg h-[266px]!"
+                                        />
+                                        <div
+                                            className="absolute top-0 w-full h-[266px] pointer-events-none z-20 rounded-b-lg"
+                                            style={{
+                                                background: `linear-gradient(to top, rgba(17, 17, 17, 0.8) 0%, rgba(17, 17, 17, 0.5) 15%, rgba(17, 17, 17, 0) 20%)`,
+                                            }}
+                                        >
+                                            <div className="absolute bottom-2 left-0 right-0">
+                                                <p className="text-white font-semibold text-center">{project.name}</p>
+                                                <p className="text-white text-xs text-center">{`${project.location[project.location.length - 1]}, ${project.district}`}</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
+                                ))
+                            }
+                        </div>
+                    )
+                }
+                {
+                    showNotFound && (
+                        <div className="p-10 shadow-lg shadow-gray-400 rounded-lg min-[500px]:w-[450px] mx-auto">
+                            <p className="text-xl text-red-500 font-bold text-center mt-4">No project found with your selected options.</p>
+                            <p className="text-gray-800 text-center mt-4">Please try with different options.</p>
+                            <div className="flex justify-center mt-6">
+                                <button 
+                                    className="w-16 rounded-lg shadow-lg p-2 cursor-pointer bg-[#F3692A] text-white font-bold"
+                                    onClick={() => setAllProjects(projects)}
+                                >OK</button>
                             </div>
-                        ))
-                    }
-                </div>
+                        </div>
+                    )
+                }
             </div>
 
             {/* Modal/Dialog */}
