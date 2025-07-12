470da0089a0b8beb68ba37bbc7e57ecd6d361ecd
ahsan <makabir98@yahoo.com>
2025-07-12 11:18:02 +0600

    some projects hide

--- app/components/Stat.jsx
+++ app/components/Stat.jsx
@@ -5,8 +5,8 @@ const Stat = () => {
     return (
         <div className="bg-[#6C6C6C]/10 w-full py-12 text-center px-6 sm:px-8 md:px-16 lg:px-20">
             <div className="flex flex-wrap xl:flex-nowrap gap-y-6 md:gap-y-8 xl:max-w-7xl xl:mx-auto">
-                <StatElement data="20+" description={<>Years of<br />Experience</>} />
-                <StatElement data="40+" description={<>Projects<br />Completed</>} />
+                <StatElement data="15+" description={<>Years of<br />Experience</>} />
+                <StatElement data="100+" description={<>Projects<br />Completed</>} />
                 <StatElement data="2M+" description={<>Square Feet Area<br />Were Built</>} />
                 <StatElement data="200K+" description={<>Square Feet Area<br />On Going</>} />
             </div>
